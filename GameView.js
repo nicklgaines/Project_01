6909d5bd3a44dbf6f4f1aba968bc28d30bb1eacf
Nick Gaines <nickgaines19@gmail.com>
2016-09-01 14:28:46 -0400

    create DOM manipulation and MVP with working choices but broken endings

---
+++ GameView.js
@@ -0,0 +1,28 @@
+
+
+const context = document.querySelector("#context");
+const text = document.querySelector("#text");
+const choices = document.querySelector("#choices");
+
+
+const newContext = function newContext(media) {
+  context.style.backgroundImage = 'url(" + media + ")';
+};
+
+const newText = function newText(words) {
+  text.innerHTML = words.replace();
+};
+
+const newChoices = function(choicesList) {
+  choices.innerHTML = "";
+  for (let i = 0; i < choicesList.length; i++) {
+    choices.innerHTML += "<button onClick="+choicesList[i][1]+">"
+                          + choicesList[i][0] + "</button>";
+  }
+};
+
+const moveTo = function(room) {
+  newContext(room.context)
+  newText(room.text)
+  newChoices(room.choices)
+};
--- events.js
+++ events.js
@@ -1,38 +1,38 @@
 let room = {};
 room = {
   one: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'When Joy came to a set of two open doors, she walked through the door to her left.',
-    buttons: [['(OBEY)Enter the door on your left', 'moveTo(room.two)'],
+    choices: [['(OBEY)Enter the door on your left', 'moveTo(room.two)'],
               ['(DISOBEY)Enter the door on your right', 'moveTo(room.three)']],
   },
   two: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'Instead of class I\'m taking you to see a pretty rainbow. WHOA! Watch the maelstrom.',
-    buttons: [['(OBEY)Skip towards the sparkling rainbow', 'moveTo(room.four)'],
+    choices: [['(OBEY)Skip towards the sparkling rainbow', 'moveTo(room.four)'],
               ['(DISOBEY)Wander towards the terrifying maelstrom', 'moveTo(room.five)']],
   },
   three: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'I see you are confused. I said left. You went right. This must be some mistake.',
-    buttons: [['(OBEY)Get back to the main story[Take the open door]', 'moveTo(room.two)'],
+    choices: [['(OBEY)Get back to the main story[Take the open door]', 'moveTo(room.two)'],
               ['(DISOBEY)Continue frivolous exploration', 'moveTo(room.six)']],
   },
   four: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'What a wonderful rainbow!',
-    buttons: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
+    choices: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
   },
   five: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'Pro tip: Avoid the maelstrom. Why didn\'t you listen? [YOU ARE VERY DEAD].',
-    buttons: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
+    choices: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
   },
 
   six: {
-    image: '', // two doors
+    context: '', // two doors
     text: 'Oh-ho how droll. Let\'s see how much you like ignoring me when THIS happens! [YOU ARE DEAD]',
-    buttons: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
+    choices: ['THE END IS NEVER THE END', 'moveTo(room.one)'],
   },
 };
 
